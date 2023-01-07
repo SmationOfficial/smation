81ee95c9a4220c06f02d639726389019c8be80a4
Smation <106820377+SmationOfficial@users.noreply.github.com>
2023-01-07 08:49:10 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,26 @@
+function adduser()
+{
+
+   window.location= "hacker.html";
+    
+}
+function otherpage()
+{
+   window.location= "paint.html";
+}
+function nextpage()
+{
+   window.location= "math_game.html";
+}
+function anotherpage()
+{
+   window.location= "typing.html";
+}
+function secondpage()
+{
+   window.location= "filter.html";
+}
+function thirdpage()
+{
+   window.location= "guess_the_word.html";
+}
